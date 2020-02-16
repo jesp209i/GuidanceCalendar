d0bfb81dcb92230b508e5f9d5671ab49d2b307c0
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-16 13:06:15 +0100

    Persistence interfaces

---
+++ GuidanceCalendar.Domain/Interfaces/Persistence/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using GuidanceCalendar.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GuidanceCalendar.Domain.Interfaces.Persistence
+{
+    public interface IRepository<T> where T: Entity
+    {
+        Task<T> AddAsync(T entity);
+        Task<T> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> ListAsync();
+        Task Update(T entityToUpdate);
+    }
+}
---
+++ GuidanceCalendar.Domain/Interfaces/Persistence/ITimeslotRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuidanceCalendar.Domain.Interfaces.Persistence
+{
+    public interface ITimeslotRepository : IRepository<Timeslot>
+    {
+    }
+}
