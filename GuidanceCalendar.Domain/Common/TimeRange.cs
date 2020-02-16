c7a403124fdfc6de0d325ec5493d2cb17a486649
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-16 13:05:55 +0100

    TimeRange changes to be an abstract class and moved from ValueObject

---
+++ GuidanceCalendar.Domain/Common/TimeRange.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuidanceCalendar.Domain.Common
+{
+    public abstract class TimeRange : Entity
+    {
+        public DateTimeOffset StartTime { get; protected set; }
+        public DateTimeOffset EndTime { get; protected set; }
+        public TimeSpan Duration { get=> EndTime.Subtract(StartTime); }
+        protected void TimeRangeGuard(DateTimeOffset start, DateTimeOffset end)
+        {
+            if (end < start) throw new ArgumentOutOfRangeException("StartTime must be before EndTime");
+        }
+        public void OverlapGuard(TimeRange t2)
+        {
+            if ((this.StartTime < t2.EndTime) && (this.EndTime > t2.StartTime)) throw new Exception("Dont overlap");
+        }
+        public void BoundaryGuard(TimeRange tr)
+        {
+            if (tr.StartTime < StartTime && tr.EndTime > EndTime) throw new Exception("Stay within boundary");
+        }
+    }
+}
