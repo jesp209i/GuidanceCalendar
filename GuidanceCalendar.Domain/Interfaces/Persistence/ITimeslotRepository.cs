a095889298683805e58bed82b28b085d8e569e94
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-16 21:33:02 +0100

    Repoimplementation

--- GuidanceCalendar.Domain/Interfaces/Persistence/ITimeslotRepository.cs
+++ GuidanceCalendar.Domain/Interfaces/Persistence/ITimeslotRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GuidanceCalendar.Domain.Interfaces.Persistence
 {
     public interface ITimeslotRepository : IRepository<Timeslot>
     {
+        Task<Timeslot> GetTimeslotWithBookingsById(Guid id);
     }
 }
--- GuidanceCalendar.Persistence/Repositories/TimeslotRepository.cs
+++ GuidanceCalendar.Persistence/Repositories/TimeslotRepository.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GuidanceCalendar.Persistence.Repositories
 {
@@ -11,6 +12,11 @@ namespace GuidanceCalendar.Persistence.Repositories
     {
         public TimeslotRepository(GuidanceCalendarContext dbContext) : base(dbContext)
         {
+        }
+
+        public async Task<Timeslot> GetTimeslotWithBookingsById(Guid id)
+        {
+            return await dbSet.Include(ts => ts.Bookings).FirstOrDefaultAsync(ts => ts.Id == id);
         }
     }
 }
