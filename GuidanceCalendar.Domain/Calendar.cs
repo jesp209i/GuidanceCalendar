b1a66364cfdcec055d470205b0230827e62e1baf
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-16 13:07:26 +0100

    Hold removed - all test has previously been removed

--- GuidanceCalendar.Domain/Calendar.cs
+++ GuidanceCalendar.Domain/Calendar.cs
@@ -8,18 +8,12 @@ namespace GuidanceCalendar.Domain
     {
         public string Name { get; private set; }
         public string Description { get; private set; }
-        public ICollection<Hold> Holds { get; private set; }
         public ICollection<Timeslot> AvailableTimeslots { get; private set; }
         public Calendar(string name, string description = "")
         {
-            Holds = new List<Hold>();
             AvailableTimeslots = new List<Timeslot>();
             Name = name;
             Description = description;
-        }
-        public void AddHold(Hold hold)
-        {
-            Holds.Add(hold);
         }
     }
 }
