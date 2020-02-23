111063cb838d53c789ab6d30aa3e3bfa4cadc707
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-23 22:23:04 +0100

    Repository implementations changed to reflect changes in interfaces

--- GuidanceCalendar.Persistence/Repositories/TeacherRepository.cs
+++ GuidanceCalendar.Persistence/Repositories/TeacherRepository.cs
@@ -1,5 +1,5 @@
-﻿using GuidanceCalendar.Domain;
-using GuidanceCalendar.Domain.Interfaces.Persistence;
+﻿using GuidanceCalendar.Persistence.DAO;
+using GuidanceCalendar.Persistence.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace GuidanceCalendar.Persistence.Repositories
 {
-    public class TeacherRepository : BaseRepository<Teacher>, ITeacherRepository
+    public class TeacherRepository : BaseRepository<TeacherDao>, ITeacherRepository
     {
         public TeacherRepository(GuidanceCalendarContext dbContext) : base(dbContext)
         {
         }
 
-        public async Task<Teacher> GetTeacherWithTimeslotsById(Guid id)
+        public async Task<TeacherDao> GetTeacherWithTimeslotsById(Guid id)
         {
             return await dbSet.Where(x => x.Id == id).Include(x => x.AvailableTimeslots).FirstOrDefaultAsync();
 
--- GuidanceCalendar.Persistence/Repositories/TimeslotRepository.cs
+++ GuidanceCalendar.Persistence/Repositories/TimeslotRepository.cs
@@ -1,20 +1,18 @@
-﻿using GuidanceCalendar.Domain;
-using GuidanceCalendar.Domain.Interfaces.Persistence;
+﻿using GuidanceCalendar.Persistence.DAO;
+using GuidanceCalendar.Persistence.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GuidanceCalendar.Persistence.Repositories
 {
-    public class TimeslotRepository : BaseRepository<Timeslot>, ITimeslotRepository
+    public class TimeslotRepository : BaseRepository<TimeslotDao>, ITimeslotRepository
     {
         public TimeslotRepository(GuidanceCalendarContext dbContext) : base(dbContext)
         {
         }
 
-        public async Task<Timeslot> GetTimeslotWithBookingsById(Guid id)
+        public async Task<TimeslotDao> GetTimeslotWithBookingsById(Guid id)
         {
             return await dbSet.Include(ts => ts.Bookings).FirstOrDefaultAsync(ts => ts.Id == id);
         }
