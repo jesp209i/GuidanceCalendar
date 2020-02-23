500cb0b0771f2e73e236c8e4731f7d41ad0bde56
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-23 22:21:08 +0100

    Repository interfaces moved to persistence, and changed to use DAO object instead of Domain Objects

--- GuidanceCalendar.Persistence/Interfaces/IRepository.cs
+++ GuidanceCalendar.Persistence/Interfaces/IRepository.cs
@@ -1,11 +1,11 @@
-﻿using GuidanceCalendar.Domain.Common;
+﻿using GuidanceCalendar.Persistence.DAO.Common;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace GuidanceCalendar.Domain.Interfaces.Persistence
+namespace GuidanceCalendar.Persistence.Interfaces
 {
-    public interface IRepository<T> where T: Entity
+    public interface IRepository<T> where T: AbstractEntityDao
     {
         Task<T> AddAsync(T entity);
         Task<T> GetByIdAsync(Guid id);
---
+++ GuidanceCalendar.Persistence/Interfaces/ITimeslotRepository.cs
@@ -0,0 +1,13 @@
+﻿using GuidanceCalendar.Persistence.DAO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuidanceCalendar.Persistence.Interfaces
+{
+    public interface ITimeslotRepository : IRepository<TimeslotDao>
+    {
+        Task<TimeslotDao> GetTimeslotWithBookingsById(Guid id);
+    }
+}
