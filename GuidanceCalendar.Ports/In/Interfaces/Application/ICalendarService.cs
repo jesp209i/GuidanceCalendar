c6cac9b26af5f9736210773fe5f64e50952689e3
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-23 22:12:35 +0100

    Port interfaces defined

---
+++ GuidanceCalendar.Ports/In/Interfaces/Application/ICalendarService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuidanceCalendar.Ports.In.Interfaces.Application
+{
+    public interface ICalendarService
+    {
+        // Commands
+        Task<Guid> CreateCalendar(
+            string name,
+            string description);
+    }
+}
---
+++ GuidanceCalendar.Ports/Out/ITeacherPersistencePort.cs
@@ -0,0 +1,15 @@
+﻿using GuidanceCalendar.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuidanceCalendar.Ports.Out
+{
+    public interface ITeacherPersistencePort
+    {
+        Task<Teacher> GetTeacherWithTimeslotsById(Guid teacherId);
+        Task<ICollection<Teacher>> ListTeachersAsync();
+        Task Update(Teacher updateTeacher);
+    }
+}
