249de9e1c1d8c339c1d38031d7564549ea1fce0f
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-23 22:28:16 +0100

    Viewmodels and adapter used in webapi

---
+++ GuidanceCalendar/Adapter/ViewmodelAdapter.cs
@@ -0,0 +1,26 @@
+﻿using GuidanceCalendar.API.Viewmodels;
+using GuidanceCalendar.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuidanceCalendar.API.Adapter
+{
+    public class ViewmodelAdapter
+    {
+        internal object ConvertToTimeslotsDTO(ICollection<Timeslot> timeslots)
+        {
+            List<TimeslotVM> vmTimeslots = new List<TimeslotVM>();
+            timeslots.ToList().ForEach(x => vmTimeslots.Add(new TimeslotVM { Id = x.Id, StartTime = x.StartTime, EndTime = x.EndTime }));
+            return vmTimeslots;
+        }
+
+        internal object ConvertToTeachersDTO(ICollection<Teacher> teachers)
+        {
+            List<TeacherVM> vmTeachers = new List<TeacherVM>();
+            teachers.ToList().ForEach(x => vmTeachers.Add(new TeacherVM { Name = x.Name, Id = x.Id }));
+            return vmTeachers;
+        }
+    }
+}
