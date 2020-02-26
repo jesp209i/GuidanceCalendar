5c58dd5646a6b533dd68f5d2f791b3ef50c55816
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-02-26 09:44:21 +0100

    More implementation in the calendar service

--- GuidanceCalendar.Application/Port/In/CalendarService.cs
+++ GuidanceCalendar.Application/Port/In/CalendarService.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using GuidanceCalendar.Ports.In.Interfaces.Application;
+using GuidanceCalendar.Application.UseCases.Calendar.Commands.CreateCalendar;
+using GuidanceCalendar.Application.UseCases.Calendar.Queries.AvaliableCalendars;
+using GuidanceCalendar.Application.UseCases.Calendar.Queries.GetCalendar;
+using GuidanceCalendar.Domain;
+using GuidanceCalendar.Ports.In.Application;
+using MediatR;
 
 namespace GuidanceCalendar.Application.Port.In
 {
-    public class CalendarService : ICalendarService
+    public class CalendarService : ApplicationService, ICalendarService
     {
-        public Task<Guid> CreateCalendar(string name, string description)
+        public CalendarService(IMediator mediator) : base(mediator)
         {
-            throw new NotImplementedException();
+        }
+
+        public async Task<Guid> CreateCalendar(string name, string description)
+        {
+            return await _mediator.Send(new CreateCalendarCommand { Name = name, Description = description });
+        }
+
+        public async Task<ICollection<Calendar>> GetAvailableCalendars()
+        {
+            return await _mediator.Send(new AvaliableCalendarsRequest());
+        }
+
+        public async Task<Calendar> GetCalendar(Guid id)
+        {
+            return await _mediator.Send(new GetCalendarQuery { Id = id });
         }
     }
 }
